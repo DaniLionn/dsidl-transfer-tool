2b2836ab0f3d58d5486a6d0ed6436f20df99f884
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-12-15 19:12:34 +0000

    change html

--- index.js
+++ index.js
@@ -8,16 +8,60 @@ const app = express();
 
 const baseURL = "https://dsidl-transfer-tool.dani-lionn.repl.co/";
 
-const baseHTML = `
-<!DOCTYPE html>
+const baseHTML = `<!DOCTYPE html>
 <head>
-   <title>dsidl transfer tool</title>
-   <link rel="icon" type="image/gif" href="https://db.universal-team.net/assets/images/icons/dsidl.gif">
-   <style>.qr{display: inline-block;padding-top: 10px;padding-right: 10px;padding-bottom: 10px;padding-left: 10px;}body {background-color: #000000;} p, a {color: #FFFFFF;font-family: Verdana;} h3 {color: #FFFFFF;font-family: Verdana;  display: inline-block;}.center {margin: auto;width: 80%;}</style>
+  <title>dsidl transfer tool</title>
+  <link rel="icon" type="image/gif" href="https://db.universal-team.net/assets/images/icons/dsidl.gif">
+  <style>
+    .qr {
+       display: inline-block;
+       padding-top: 10px;
+       padding-right: 10px;
+       padding-bottom: 10px;
+       padding-left: 10px;
+     }
+    body {
+      background-color: #000000;
+      } 
+      p, a {
+        color: #FFFFFF;
+        font-family: Verdana;
+        } 
+        h3 {
+        color: #FFFFFF;
+        font-family: Verdana;  
+        display: inline-block;
+        }
+        .center {
+        margin: auto;
+        width: 80%;
+        }
+  </style>
 </head>
 <body>
-   <div class="center"><h3 style="text-align:center;"><img src="https://db.universal-team.net/assets/images/icons/dsidl.gif" width="25" height="25" style="padding-right: 7px" >QR codes are meant to be scanned with the <a href="https://github.com/Epicpkmn11/dsidl" target="_blank">dsidl homebrew app</a> for the DSi. <img src="https://db.universal-team.net/assets/images/icons/dsidl.gif" width="25" height="25" style="padding-left: 5px" ></h3><p>Upload a file and it'll be turned into a QR Code that you can scan with dsidl to download to your DSi!</p><p>Uploaded files are deleted after being scanned and sent over.</p></div>
-   <br>`;
+  <div class="center">
+  <h3 style="text-align:center;">
+  <img src="https://db.universal-team.net/assets/images/icons/dsidl.gif" width="25" height="25" style="padding-right: 7px" >QR codes are meant to be scanned with the <a href="https://github.com/Epicpkmn11/dsidl" target="_blank">dsidl homebrew app</a> for the DSi. 
+    <img src="https://db.universal-team.net/assets/images/icons/dsidl.gif" width="25" height="25" style="padding-left: 5px" >
+  </h3>
+  <p>
+    Upload a file and it'll be turned into a QR Code that you can scan with dsidl to download to your DSi!
+  </p>
+  <p>
+    Uploaded files are deleted after being scanned and sent over.
+  </p>
+  </div>
+  <br>`;
+
+const htmlFinalPart =  `<div class="center"> 
+    <form action="/upload" method="post" enctype="multipart/form-data">
+      <input type="file" name="file" />
+      <button type="submit">Upload</button>
+    </form>
+  </div>
+  <p>Check out the <a href="https://github.com/DaniLionn/dsidl-transfer-tool">github repo</a>!
+  </p>
+</body>`
 var finalHTML = baseHTML;
 
 const options = {
@@ -74,7 +118,7 @@ app.post("/upload", upload.single("file"), (req, res) => {
     const filePath = path.join("./uploads", uploadedFile.filename);
 
     app.use(encodeURI(`/${uploadedFile.filename}`), function (req, res, next) {
-      console.log("Got request!");
+      console.log("Got request! Sending file...");
 
       res.sendFile(filePath, options, function (err) {
         if (err) {
@@ -84,12 +128,6 @@ app.post("/upload", upload.single("file"), (req, res) => {
           fs.promises.unlink(filePath);
         }
       });
-    });
-
-    app.get(encodeURI(`/${uploadedFile.filename}`), function (req, res) {
-      console.log("File sent!");
-
-      res.send("file sent");
     });
 
     createQRCode(encodeURI(uploadedFile.filename)).then(() => {
@@ -111,19 +149,11 @@ app.get("/", function (req, res) {
     finalHTML = baseHTML;
 
     res.send(
-      finalHTML +
-        `   <div class="center"> <form action="/upload" method="post" enctype="multipart/form-data">
-    <input type="file" name="file" />
-    <button type="submit">Upload</button>
-  </form></div></body>`,
+      finalHTML + htmlFinalPart,
     );
   } else {
     res.send(
-      finalHTML +
-        `   <div class="center"> <form action="/upload" method="post" enctype="multipart/form-data">
-        <input type="file" name="file" />
-        <button type="submit">Upload</button>
-      </form></div></body>`,
+      finalHTML + htmlFinalPart,
     );
   }
 });
